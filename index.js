deb62e58ef6a07c6e601205f791fd624f565bf1e
Koushal Khajuria <68377402+koushalkhajuria@users.noreply.github.com>
2023-06-15 21:23:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.post('/signup', async(req,res) => {
 
 
 
-app.listen(port,"127.0.0.1",(err) => {
+app.listen(port,(err) => {
     dbConnect()
     if(err) throw err;
     console.log(`Server is running on port ${3000}`)
